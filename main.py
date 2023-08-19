c23e0db8e5f0f12ca3cf655714bfcf445c63ec16
efeculcu <132135979+efeculcu@users.noreply.github.com>
2023-08-19 20:16:06 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-import random
-hobi = ["Santranc","Basket","kodlama" ]
-print(random.choice(hobi))
+password = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+password_len = int(input("Parolanın uzunluğunu girin: "))
+for password in range (0,password_len):
+    print (password)
+import random 
+password = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+print (random + password)
+
+

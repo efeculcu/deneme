2f6ffc5c9053466c1d795eff00e59d09f6b34f94
efeculcu <132135979+efeculcu@users.noreply.github.com>
2023-08-31 23:00:21 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,15 +11,8 @@ def gen_pass(pass_length):
     return password
 
 
-def gen_emodji():
-    emodji = ["\U0001f60", "\U0001f642", "\U0001F606", "\U0001F923"]
-    return random.choice(emodji)
-
-
 def flip_coin():
     flip = random.randint(0, 2)
     if flip == 0:
         return "HEADS"
-    else:
-        return "TAILS"
  

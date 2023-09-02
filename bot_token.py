19f394df45f0f9fc757d42b2bddf470722453e78
efeculcu <132135979+efeculcu@users.noreply.github.com>
2023-09-02 16:38:19 +0300

    Create bot_token.py

---
+++ bot_token.py
@@ -0,0 +1,4 @@
+ayarlar = {
+    "on_taki": ">", #ön takı, prefix (maybe it won't work if we call it different here)
+    "TOKEN": "MTE0NzE1NDAzMzcwMjg3NTE1Ng.GnN9uM.MwEGpS-6_s9ixCcdUL64FOncF_njEQUF4qtsxk"
+}

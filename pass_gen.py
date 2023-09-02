5e92e4636543da4a6e25d9fd0404f56eca7555b1
efeculcu <132135979+efeculcu@users.noreply.github.com>
2023-09-02 16:38:41 +0300

    Create pass_gen.py

---
+++ pass_gen.py
@@ -0,0 +1,13 @@
+import random 
+x = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+password_len = int(input("Parolanın uzunluğunu girin: "))
+password_adeti = int(input("Kaç adet şifre oluşturulsun: "))
+for y in range (0, password_len):
+ password = ""
+for y in range (0,password_adeti):
+    print (password)
+for password in range(0, password_len):
+   sifre2= random.choice(x)
+   sifre = sifre + sifre2
+print ("random sifreniz" + password)
